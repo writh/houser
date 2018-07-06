25077167ad3debcafd1dce05a1abedf33e82afe8
writh <victoria.thursday@gmail.com>
2018-07-05 20:34:49 -0600

    set up dotenv

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const controller = require('./controller');
 const port = 3005;
 const app = express();
+const massive = require('massive');
+require('dotenv').config({ path: __dirname + '/../.env', });
 app.use(bodyParser.json())
 
 
